4458d5da1ecff4c9f46acb4b8bfd490938a369da
punishthecode <phanishmurthy2001@gmail.com>
2023-08-10 10:54:31 +0530

    Changed to netlify domain

--- src/auth0-config.ts
+++ src/auth0-config.ts
@@ -1,7 +1,13 @@
+// const auth0Config = {
+//   domain: "dev-86ii2koz7pd1r1om.us.auth0.com",
+//   clientId: "wVkAXrjfWD9CYfdBakBDHTXTazLRdQWf",
+//   redirect_uri: "http://127.0.0.1:5173",
+// };
+
 const auth0Config = {
   domain: "dev-86ii2koz7pd1r1om.us.auth0.com",
   clientId: "wVkAXrjfWD9CYfdBakBDHTXTazLRdQWf",
-  redirect_uri: "http://127.0.0.1:5173",
+  redirect_uri: "https://punishthetodo.netlify.app",
 };
 
 export default auth0Config;
