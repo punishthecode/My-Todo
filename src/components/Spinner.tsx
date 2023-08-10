ae857463a4a2f01e571a6073bc97d8efa0219b5b
punishthecode <phanishmurthy2001@gmail.com>
2023-08-10 12:14:10 +0530

    Lazy Loading

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, CircularProgress } from "@mui/material";
+
+const Spinner: React.FC = () => {
+  return (
+    <Box>
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default Spinner;
